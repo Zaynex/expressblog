f0a0e337a4369fb0c2ce958e1c77b5e363b04265
zaynex <279648970@qq.com>
2016-09-29 10:30:51 +0800

    finish index

--- maizi/blog/blog/routes/index.js
+++ maizi/blog/blog/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(app) {
+  app.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+  });
+  app.get('/reg', function(req,res){
+  	res.render('reg', {title: "注册"});
+  });
+  app.post('/reg', function(req, res){
+  });
+  app.get('/login', function(req, res){
+  	res.render('login', {title: "登录"});
+  })
+  app.post('/login', function(req, res){
+  });
+  app.get('post', function(req, res){
+  	res.render('post', {title: "发表"});
+  });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+  app.post("/post", function(req, res){});
+  app.get('/logout', function(req, res){});
+};
