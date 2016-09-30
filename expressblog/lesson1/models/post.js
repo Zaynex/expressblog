a2485ab72227f233eda199339ed2bdea14686395
zaynex <279648970@qq.com>
2016-09-30 13:23:32 +0800

    add edit and delete

--- expressblog/lesson1/models/post.js
+++ expressblog/lesson1/models/post.js
@@ -52,7 +52,7 @@ Post.prototype.save = function(callback){
 };
 
 
-Post.get = function(name, callback){
+Post.getAll = function(name, callback){
 	mongodb.open(function(err, db){
 		if(err) {
 			return callback(err);
@@ -82,4 +82,118 @@ Post.get = function(name, callback){
 			});
 		});
 	});
+};
+
+Post.getOne = function(name, day, title, callback) {
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //根据用户名、发表日期及文章名进行查询
+      collection.findOne({
+        "name": name,
+        "time.day": day,
+        "title": title
+      }, function (err, doc) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        //解析 markdown 为 html
+        doc.post = markdown.toHTML(doc.post);
+        callback(null, doc);//返回查询的一篇文章
+      });
+    });
+  });
+};
+
+Post.edit = function(name, day, title, callback) {
+	mongodb.open(function(err, db){
+		if(err) {
+			return callback(err);
+		}
+		//读取posts集合
+		db.collection('posts', function(err, collection){
+			if(err) {
+				mongodb.close();
+				return callback(err);
+			}
+			collection.findOne({
+				"name": name,
+				"time.day": day,
+				"title": title
+			}, function(err, doc){
+				mongodb.close();
+				if(err) {
+					return callback(err);
+				}
+				callback(null, doc);
+			});
+		});
+	});
+};
+
+Post.update = function(name, day, title, post, callback){
+	mongodb.open(function(err, db){
+		if(err) {
+			return callback(err);
+		}
+		db.collection('posts', function(err, collection){
+			if(err) {
+				mongodb.close();
+				return callback(err);
+			}
+
+			collection.update({
+				"name": name,
+				"time.day": day,
+				"title": title
+			}, {
+				$set: {post: post}
+			}, function(err) {
+				mongodb.close();
+				if(err) {
+					return callback(err);
+				}
+				callback(null);
+			});
+		});
+	});
+};
+
+Post.remove = function(name, day, title, callback) {
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //根据用户名、日期和标题查找并删除一篇文章
+      collection.remove({
+        "name": name,
+        "time.day": day,
+        "title": title
+      }, {
+        w: 1
+      }, function (err) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null);
+      });
+    });
+  });
 };
